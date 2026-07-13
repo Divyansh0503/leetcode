f110d2cd3a6ae780723c40075d7f22667f2de9fc
Divyansh kumar singh <121854164+Divyansh0503@users.noreply.github.com>
2026-07-13 23:14:04 +0530

    Create D13.java

---
+++ D13.java
@@ -0,0 +1,20 @@
+/
+/
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=0;i<=9;i++){
+            int res=0;
+            for(int j=i+1;j<=9;j++){
+                res=res*10+j;
+
+                if(res>=low && res<=high) ans.add(res);
+
+                if(res>high) break;
+
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+}
